1560ac9eba564eb95e8c9eefd4973fdb3fa04264
Rahmad Alamsyah Nazaruddin <54619997+Alamnzr123@users.noreply.github.com>
2023-03-17 10:07:21 +0700

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // panggil package node-fetch
 import fetch from 'node-fetch';
 
+const data = 'data'
+
 const GetData = async () => {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await response.json();
@@ -8,3 +10,4 @@ const GetData = async () => {
 }
 
 GetData();
+
